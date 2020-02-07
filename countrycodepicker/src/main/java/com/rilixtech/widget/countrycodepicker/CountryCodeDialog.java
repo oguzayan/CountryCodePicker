efb1119c5e512f9c21b7697465be2ce01b37ef59
oguzayan <oguz@kukapps.com>
2020-02-07 13:58:36 +0300

    disabled focus

--- countrycodepicker/src/main/java/com/rilixtech/widget/countrycodepicker/CountryCodeDialog.java
+++ countrycodepicker/src/main/java/com/rilixtech/widget/countrycodepicker/CountryCodeDialog.java
@@ -170,11 +170,11 @@ class CountryCodeDialog extends Dialog {
       }
     });
 
-//    if (mCountryCodePicker.isKeyboardAutoPopOnSearch()) {
-//      if (mInputMethodManager != null) {
-//        mInputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
-//      }
-//    }
+      if (mCountryCodePicker.isKeyboardAutoPopOnSearch()) {
+          if (mInputMethodManager != null) {
+              mInputMethodManager.hideSoftInputFromWindow(mEdtSearch.getWindowToken(), 0);
+          }
+      }
   }
 
   /**
